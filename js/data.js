c2dd88b25d816ae08887b345313913898729a8b3
Muhammad Hakim <phakimb33@gmail.com>
2021-12-02 20:21:17 +0700

    Add New Data

--- js/data.js
+++ js/data.js
@@ -1,13 +1,18 @@
 // List Data
 export let data = [
     {
-        link: "https://flexiple.com/",
-        title: "Sistem Informasi",
-        tech: ["CSS", "Bulma"]
+        link: "https://puskesmasjabar.000webhostapp.com/",
+        title: "Sistem Informasi Puskesmas",
+        tech: ["Codeigniter 3", "Bulma", "jquery", "SwipertJs"]
     },
     {
-        link: "https://dmitripavlutin.com/",
-        title: "Sistem",
-        tech: ["CSS", "Bulma", "Jqery"]
+        link: "https://muhammadhakim33.github.io/musicPlayer/",
+        title: "Simple Music Player",
+        tech: ["CSS", "Vanilla Javascript"]
     },
+    {
+        link: "https://muhammadhakim33.github.io/thekrustykrab/",
+        title: "Landing Page The Krusty Krab",
+        tech: ["CSS", "Vanilla Javascript"]
+    }
 ];
