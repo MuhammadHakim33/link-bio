293be5a3b4699d7d52bbfcdf15f7839f881dd63e
Muhammad Hakim <phakimb33@gmail.com>
2021-12-02 20:24:36 +0700

    Data revisi

--- js/data.js
+++ js/data.js
@@ -3,16 +3,16 @@ export let data = [
     {
         link: "https://puskesmasjabar.000webhostapp.com/",
         title: "Sistem Informasi Puskesmas",
-        tech: ["Codeigniter 3", "Bulma", "jquery", "SwipertJs"]
+        tech: ["Ci3", "Bulma", "jquery", "SwipertJs"]
     },
     {
         link: "https://muhammadhakim33.github.io/musicPlayer/",
         title: "Simple Music Player",
-        tech: ["CSS", "Vanilla Javascript"]
+        tech: ["CSS", "Javascript"]
     },
     {
         link: "https://muhammadhakim33.github.io/thekrustykrab/",
         title: "Landing Page The Krusty Krab",
-        tech: ["CSS", "Vanilla Javascript"]
+        tech: ["CSS", "Javascript"]
     }
 ];
